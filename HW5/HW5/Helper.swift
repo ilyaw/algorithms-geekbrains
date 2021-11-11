3c3ec940ee6d0c607dcf02f431e00c71d3a13c70
ilya rudenko <webilya29@gmail.com>
2021-11-11 17:22:07 +0300

    Implement hash functions

--- HW5/HW5/Helper.swift
+++ HW5/HW5/Helper.swift
@@ -1,6 +1,6 @@
 //
 //  Helper.swift
-//  HW5
+//  HW6
 //
 //  Created by Ilya on 08.11.2021.
 //
@@ -9,10 +9,4 @@ import Foundation
 
 func readLine<T: LosslessStringConvertible>(as type: T.Type) -> T? {
     return readLine().flatMap(type.init(_:))
-}
-
-extension Dictionary where Value: Equatable {
-    func findKey(forValue val: Value) -> Key? {
-        return first(where: { $1 == val })?.key
-    }
 }
