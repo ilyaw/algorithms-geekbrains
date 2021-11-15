e4e719c0fd172c12b9079ee5ac80003b759468bf
ilya rudenko <webilya29@gmail.com>
2021-11-15 15:33:32 +0300

    Print adjacency matrix

---
+++ HW7/HW7/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  HW7
+//
+//  Created by Ilya on 15.11.2021.
+//
+
+import Foundation
+
+print("Номер задачи 1...2:", terminator: " ")
+
+switch readLine(as: Int.self) {
+case 1:
+    solution1()
+case 2:
+//    solution2()
+    break
+default:
+    break
+}
